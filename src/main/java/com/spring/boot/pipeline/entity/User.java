fc68af2da42d157555b3c9374a853576a44a7ebb
Andre <andreluisv8@gmail.com>
2025-10-13 16:52:24 -0300

    Created class User

---
+++ src/main/java/com/spring/boot/pipeline/entity/User.java
@@ -0,0 +1,49 @@
+package com.spring.boot.pipeline.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
